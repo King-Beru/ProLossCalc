a24273a48b57ea5f437e5b11a4f95b5b6eacd3a6
King-Beru <harshit.1kapoor1@gmail.com>
2025-04-20 14:51:42 +0530

    Updated: Added Commments

--- PLC.py
+++ PLC.py
@@ -1,20 +1,31 @@
-a=float(input("Enter current price of TCS Shares: "))
-b=float(input("Enter current prive of Railtel Shares: "))
-print(a*15," is current total price of TCS shares.")
-print(b*20," is the current total price of Railtel shares.")
-x=(a*15)-57474.75
-y=(b*20)-8374
+#PROFIT AND LOSS CALCULATOR
+
+#Used  variables to easily take input from user.
+a=float(input("Enter current price of Share 1 : "))
+p=float(input("Enter initial price of Share 1 bought: "))
+q1=int(input("Enter Qty of Share1.: "))
+b=float(input("Enter current prive of Share 2 : "))
+q=float(input("Enter initial price of Share 2 bought: "))
+q2=int(input("Enter Qty of Share 2.: "))
+
+#Used operators to check the profit and loss
+print(a*q1," is current total price of Share 1 shares.")
+print(b*q2," is the current total price of Share 2 shares.")
+x=(a*q1)-(p*q1)
+y=(b*q2)-(q*q2)
+
+#Nested if statements to print the profit and loss statements of each share.
 if x>0:
     if y>0  :
-        print(x," is Profit of TCS and ",y," is the Profit of Railtel shares")
+        print(x," is Profit of Share 1 and ",y," is the Profit of Share 2 shares")
         print(x+y," is total PROFIT")
     else:
-        print(x," is Profit of TCS and ",y," is the LOSS of Railtel shares")
+        print(x," is Profit of Share 1 and ",y," is the LOSS of Share 2 shares")
         print(x+y," is total PROFIT")
 else:
     if y>0:
-        print(x," is LOSS of TCS and ",y," is the Profit of Railtel shares")
+        print(x," is LOSS of Share 1 and ",y," is the Profit of Share 2 shares")
         print(x+y," is total PROFIT")
     else:
-        print(x," is LOSS of TCS and ",y," is the LOSS of Railtel shares")
+        print(x," is LOSS of Share 1 and ",y," is the LOSS of Share 2 shares")
         print(x+y," is total LOSS")
